73588eaee5636dd7d29ea09cfadffd8fd2419b52
chantelle711 <elenachantelle@gmail.com>
2019-11-15 11:03:23 -0800

    first commit

---
+++ python_import/helper.py
@@ -0,0 +1,5 @@
+def saludos(nombre, apellido):
+    return f'Hola {nombre} {apellido}'
+
+
+# print(saludos('chantelle', 'leany'))
---
+++ python_import/main.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0, './libs')
+import helper as h
+
+def render():
+    print(h.saludos('chantelle', 'leany'))
+
+
+render()
